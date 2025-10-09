813a2ffacec989e9eb8fc1db0fb308eb2b568ada
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-10-09 13:51:45 +0100

    fix(version): improve version string generation
    Prevent debug.ReadBuildInfo from overwriting version, commit, and date
    values that have been set via ldflags, ensuring explicit build
    information takes precedence.
    
    Additionally, simplify Go module pseudo-version strings for development
    builds, formatting them as 'dev-abcdef' for clearer display.

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"runtime/debug"
+	"strings"
 	"time"
 )
 
@@ -12,22 +13,42 @@ var (
 )
 
 func init() {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		if info.Main.Version != "(devel)" && info.Main.Version != "" {
-			version = info.Main.Version
-		}
-
-		for _, setting := range info.Settings {
-			switch setting.Key {
-			case "vcs.revision":
-				if len(setting.Value) > 7 {
-					commit = setting.Value[:7]
-				} else if setting.Value != "" {
-					commit = setting.Value
+	// Only use build info if version wasn't set via ldflags
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			// Try to get version from build info
+			if info.Main.Version != "(devel)" && info.Main.Version != "" {
+				// Clean up pseudo-versions for better display
+				v := info.Main.Version
+				// If it's a pseudo-version like v0.0.0-20210101120000-abcdef123456
+				// Just show "dev-abcdef" instead
+				if strings.HasPrefix(v, "v0.0.0-") && strings.Contains(v, "-") {
+					parts := strings.Split(v, "-")
+					if len(parts) >= 3 {
+						version = "dev-" + parts[2][:7]
+					}
+				} else {
+					version = v
 				}
-			case "vcs.time":
-				if t, err := time.Parse(time.RFC3339, setting.Value); err == nil {
-					date = t.Format("02/01/2006")
+			}
+
+			// Get commit and date from VCS info
+			for _, setting := range info.Settings {
+				switch setting.Key {
+				case "vcs.revision":
+					if commit == "unknown" {
+						if len(setting.Value) > 7 {
+							commit = setting.Value[:7]
+						} else if setting.Value != "" {
+							commit = setting.Value
+						}
+					}
+				case "vcs.time":
+					if date == "unknown" {
+						if t, err := time.Parse(time.RFC3339, setting.Value); err == nil {
+							date = t.Format("02/01/2006")
+						}
+					}
 				}
 			}
 		}
